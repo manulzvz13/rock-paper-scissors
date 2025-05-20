4a0ad6052948bdab4e226421bd2cf50667c3da32
manulzvz13 <manulzvz@gmail.com>
2025-05-20 08:05:08 -0600

    feat: Create playRound algorithm to play with user input

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ let getHumanChoice = prompt('Choose your weapon: ');
 
 let computerScore = 0;
 let humanScore = 0;
+
+function playRound (computerChoice, humanChoice) {
+    humanChoice = humanChoice.toUpperCase();
+    // Rock vs Paper = Wins Paper *
+    // Rock vs Rock = Nobody wins *
+    // Rock vs Scissors = Wins Rock *
+    // Paper vs Paper = Nobody wins *
+    // Paper vs Scissors = Wins Scissors *
+    // Scissors vs Scissors = Nobody wins *
+    if(humanChoice == 'ROCK' && computerChoice == 1 || humanChoice == 'PAPER' && computerChoice == 2 || humanChoice == 'SCISSORS' && computerChoice == 3) {
+        console.log('Nobody wins!');
+        return;
+    } else if (humanChoice == 'ROCK' && computerChoice == 2) {
+        console.log('Computer wins!');
+        return computerChoice++;
+    } else if (humanChoice == 'PAPER' && computerChoice == 1) {
+        console.log('Human wins!');
+        return humanChoice;
+    } else if (humanChoice == 'ROCK' && computerChoice == 3) {
+        console.log('Human wins!');
+        return humanChoice++;
+    } else if (humanChoice == 'SCISSORS' && computerChoice == 1) {
+        console.log('Computer wins!');
+        return computerChoice;
+    } else if (humanChoice == 'PAPER' && computerChoice == 3) {
+        console.log('Computer wins!');
+        return computerChoice++;
+    } else if (humanChoice == 'SCISSORS' && computerChoice == 2) {
+        console.log('Human wins!');
+        return humanChoice;
+    } 
+}
+
+let computerSelection = getComputerChoice();
+let humanSelection = getHumanChoice;
+
+playRound(computerSelection, humanSelection);
