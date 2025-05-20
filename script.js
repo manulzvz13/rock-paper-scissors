e16698ebf298bd94f23c09ad807732e38a44953d
manulzvz13 <manulzvz@gmail.com>
2025-05-20 07:21:45 -0600

    feat: Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello world');
+// Plan or pseudocode your solution.
+// Write the code.
+// Test your code to make sure it works
+
+function getComputerChoice () {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
