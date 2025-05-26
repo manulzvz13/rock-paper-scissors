9c3a2be792126bc4eb4ccee137923d7a23105706
manulzvz13 <manulzvz@gmail.com>
2025-05-25 21:38:28 -0600

    feat: Add div for displaying human and computer scores via DOM methods.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-// Plan or pseudocode your solution.
-// Write the code.
-// Test your code to make sure it works
+// DOM Manipulation Elements
+
+const menu = document.querySelector("#menu");
+const humanSpace = document.querySelector("#humanSpan");
+const computerSpace = document.querySelector("#computerSpan");
+const humanResult = document.createElement("p");
+const computerResult = document.createElement("p");
+
+menu.addEventListener("click", (event) => {
+  let target = event.target;
+  playRound(getComputerChoice(), target.id);
+  humanResult.textContent = `${humanScore}`;
+  computerResult.textContent = `${computerScore}`;
+  humanSpace.appendChild(humanResult);
+  computerSpace.appendChild(computerResult);
+});
 
 function getComputerChoice() {
   return Math.floor(Math.random() * 3) + 1;
@@ -13,60 +26,57 @@ function playRound(computerChoice, humanChoice) {
     (humanChoice == "PAPER" && computerChoice == 2) ||
     (humanChoice == "SCISSORS" && computerChoice == 3)
   ) {
+    console.log(`human selects: ${humanChoice}`);
+    console.log(`computer selects : ${computerChoice}`);
     console.log("Nobody wins!");
-    return;
+    return 0;
   } else if (humanChoice == "ROCK" && computerChoice == 2) {
+    console.log(`human selects: ${humanChoice}`);
+    console.log(`computer selects : ${computerChoice}`);
     console.log("Computer wins!");
     return computerScore++;
   } else if (humanChoice == "PAPER" && computerChoice == 1) {
+    console.log(`human selects: ${humanChoice}`);
+    console.log(`computer selects : ${computerChoice}`);
     console.log("Human wins!");
     return humanScore++;
   } else if (humanChoice == "ROCK" && computerChoice == 3) {
+    console.log(`human selects: ${humanChoice}`);
+    console.log(`computer selects : ${computerChoice}`);
     console.log("Human wins!");
     return humanScore++;
   } else if (humanChoice == "SCISSORS" && computerChoice == 1) {
+    console.log(`human selects: ${humanChoice}`);
+    console.log(`computer selects : ${computerChoice}`);
     console.log("Computer wins!");
     return computerScore++;
   } else if (humanChoice == "PAPER" && computerChoice == 3) {
+    console.log(`human selects: ${humanChoice}`);
+    console.log(`computer selects : ${computerChoice}`);
     console.log("Computer wins!");
     return computerScore++;
   } else if (humanChoice == "SCISSORS" && computerChoice == 2) {
+    console.log(`human selects: ${humanChoice}`);
+    console.log(`computer selects : ${computerChoice}`);
     console.log("Human wins!");
     return humanScore++;
   }
 }
 
-function playGame(num) {
-  for (i = 0; i < num; i++) {
-    let getHumanChoice = prompt("Choose your weapon: ");
-    let humanSelection = getHumanChoice;
-    let computerSelection = getComputerChoice();
-    playRound(computerSelection, humanSelection);
-  }
-  if (humanScore > computerScore) {
-    console.log("Humans dominate!");
-  } else {
-    console.log("Computers are coming for us!");
-  }
-}
+// function playGame(num) {
+//   // for (i = 0; i < num; i++) {
+//   //   let getHumanChoice = prompt("Choose your weapon: ");
+//   //   let humanSelection = getHumanChoice;
+//   //   let computerSelection = getComputerChoice();
+//   //   playRound(computerSelection, humanSelection);
+//   // }
+//   if (humanScore > computerScore) {
+//     console.log("Humans dominate!");
+//   } else {
+//     console.log("Computers are coming for us!");
+//   }
+// }
 
 let computerScore = 0;
 let humanScore = 0;
-
 // playGame(5);
-
-const menu = document.querySelector("#menu");
-menu.addEventListener('click', (event) => {
-  let target = event.target;
-  switch(target.id) {
-    case 'rock':
-      console.log('human selects rock');
-      break;
-    case 'paper':
-      console.log('human selects paper');
-      break;
-    case 'scissors':
-      console.log('human selects scissors');
-      break;
-  }
-});
